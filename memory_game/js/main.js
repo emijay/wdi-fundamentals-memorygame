3333a1fff1dc9c3d4ceccefaa6d6298bdcb775a5
emijay <hilmi.johari@yahoo.com.sg>
2019-05-27 21:06:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 
-console.log('up and running!')
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped" + " " + cardOne);
+
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again')
+	}
+} 
